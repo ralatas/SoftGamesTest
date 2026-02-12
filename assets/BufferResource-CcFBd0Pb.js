aca7abba6cb093cd3b57282c2dd03d70d60a8c5b
Stanislav <stanislav.spirkin@yandex.ru>
2026-02-12 18:08:50 +0500

    Updates

--- assets/BufferResource-CcFBd0Pb.js
+++ assets/BufferResource-CcFBd0Pb.js
@@ -1,4 +1,4 @@
-import{w as U,g,c,u as S,B as _,d as m,E as I,e as p}from"./index-Bfw4OIUi.js";const x={name:"local-uniform-bit",vertex:{header:`
+import{w as U,g,c,u as S,B as _,d as m,E as I,e as p}from"./index-BPICewly.js";const x={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
--- assets/webworkerAll-BqQmY4pf.js
+++ assets/webworkerAll-BqQmY4pf.js
@@ -1,4 +1,4 @@
-import{G as R,b as S,i as b,M as P,K as I,z as B,$ as T,T as _,ai as O,R as A,w as C,_ as z,p as k}from"./index-Bfw4OIUi.js";import{F as E}from"./Filter-1YnH25ZN.js";var U=`in vec2 aPosition;
+import{G as R,b as S,i as b,M as P,K as I,z as B,$ as T,T as _,ai as O,R as A,w as C,_ as z,p as k}from"./index-BPICewly.js";import{F as E}from"./Filter-BVTLjz6M.js";var U=`in vec2 aPosition;
 out vec2 vTextureCoord;
 
 uniform vec4 uInputSize;
